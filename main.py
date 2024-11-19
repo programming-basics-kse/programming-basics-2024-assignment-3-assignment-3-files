a4a508d5ad20eaf6455b9bd315b0e4772da0f5b1
senatede <bhupalo@kse.org.ua>
2024-11-19 22:51:53 +0200

    bit cleaner

--- main.py
+++ main.py
@@ -15,32 +15,33 @@ data = [{data[0][n]: int(field) if field.isdigit() else field for n, field in en
 def format_left(string, length): return string + " "*(length - len(string))
 def format_center(string, length): return ' '*((n := length - len(string))//2) + string + ' '*((n+n%2)//2)
 
-if config["medals"]:
-    config["country"], config["year"] = config["medals"][0], int(config["medals"][1])
-    try:
-        entries = [entry for entry in data if config["country"] in (entry['Team'], entry['NOC']) and
-                   entry['Year'] == config["year"] and entry['Medal'] != 'NA']
-        if not entries: raise ValueError("No entries found")
-        first10 = [(e["Name"], e["Sport"], e["Medal"]) for e in entries[0:10 if len(entries) >= 10 else len(entries)]]
-        max_len = [max(len(x[i]) for x in first10) for i in range(2)]
-        string_len = sum(max_len) + 19
+def handle_medals_arg(data_: list, country: str, year: int):
+    entries = [entry for entry in data_ if country in (entry['Team'].split('-')[0], entry['NOC']) and
+               entry['Year'] == year and entry['Medal'] != 'NA']
+    if not entries: raise ValueError("No entries found")
+    first10 = [(e["Name"], e["Sport"], e["Medal"]) for e in entries[0:10 if len(entries) >= 10 else len(entries)]]
+    max_len = [max(len(x[i]) for x in first10) for i in range(3)]
+    string_len = sum(max_len) + 15
 
-        output = f"""
-    {format_center('Medalists:', string_len)}
-    {'-'*string_len}
-     №   | {' | '.join((format_center(x, max_len[i]) for i, x in enumerate(('Name', 'Sport'))))} | Medal
-    {'-'*string_len}
-    {'\n'.join((f' {n+1}. {' '*(n < 9)}| ' +
-                ' | '.join((*(format_left(x[i], max_len[i]) for i in range(2)), x[2])) for n, x in enumerate(first10)))}
-    {'-'*string_len}
-     Total medals: {"Gold: {Gold}, Silver: {Silver}, Bronze: {Bronze}".format(**{
-        medal: [entry['Medal'] for entry in entries].count(medal) for medal in ['Gold', 'Silver', 'Bronze']})}
-    """
-        print(output)
-        if config["output"]:
-            print(output, file=open(config['output'], 'w'))
-    except Exception as e:
-        print(e)
+    return f"""
+{format_center('Medalists:', string_len)}
+{'-' * string_len}
+ №   | {' | '.join((format_center(x, max_len[i]) for i, x in enumerate(('Name', 'Sport'))))} | Medal  |
+{'-' * string_len}
+{'\n'.join((f' {n + 1}. {' ' * (n < 9)}| ' +
+            ' | '.join((*(format_left(x[i], max_len[i]) for i in range(3)), '')) for n, x in enumerate(first10)))}
+{'-' * string_len}
+Total medals: {"Gold: {Gold}, Silver: {Silver}, Bronze: {Bronze}".format(**{
+    medal: [entry['Medal'] for entry in entries].count(medal) for medal in ['Gold', 'Silver', 'Bronze']})}"""
+
+def output(text: str):
+    print(text)
+    if config["output"]:
+        print(text, file=open(config['output'], 'w'))
+
+if config["medals"]:
+    output_text = handle_medals_arg(data, config["medals"][0], int(config["medals"][1]))
+    output(output_text)
 elif config["total"]:
     pass
 elif config["overall"]:
