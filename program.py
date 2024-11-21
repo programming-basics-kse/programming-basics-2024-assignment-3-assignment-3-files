b5ed1578fa61570620af634fa09482996c15a53d
Vladyslav Tkachenko <vladyslavtkachenko@MacBook-Air-Vladyslav.local>
2024-11-21 14:32:52 +0200

    update

--- program.py
+++ program.py
@@ -3,13 +3,24 @@ with open('Olympic Athletes - athlete_events.csv', 'rt') as file:
     people = []
     for line in file:
         line = line.strip()
-        split = line.split(',')  # Розбиваємо рядок
-        #if len(split) == 4:  # Перевіряємо, чи є всі потрібні частини
-        a = split[0]
-        b = split[1]
-        c = split[2]
-        d = split[3]
+        split = line.split(',')
+        id = split[0]
+        Name = split[1]
+        Sex = split[2]
+        Age = split[3]
+        Height = split[4]
+        Weight = split[5]
+        Team =split[6]
+        noc = split[7]
+        Games = split[8]
+        Year = split[9]
+        Season = split[10]
+        City = split[11]
+        Sport = split[12]
+        Event = split[13]
+        Medal = split[14]
         people.append((1, 2, 3, 4)) 
+        
         print(people)
 
             
