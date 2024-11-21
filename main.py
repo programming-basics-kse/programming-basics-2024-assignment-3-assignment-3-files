67b883aed7966e8f65a9f10dd9ece3b5c76f4542
mrosliakov <mrosliakov@kse.org.ua>
2024-11-22 01:08:13 +0200

    basic solution of task1 was done.

--- main.py
+++ main.py
@@ -19,6 +19,23 @@ with open("noc_regions.csv", 'rt') as noc_regions_csv_file:
     header_noc_regions = get_header_indexes(next(noc_regions_csv_reader))
     for row in noc_regions_csv_reader:
         rows_noc_regions.append(row)
+#
+# print(header_athletes_events)
+# print(header_noc_regions)
 
-print(header_athletes_events)
-print(header_noc_regions)
+def get_medals(noc:str, year:int):
+    noc = noc.upper().strip()
+    winners = []
+    medals = {"Gold": 0, "Silver": 0, "Bronze": 0}
+    counter = 0
+    for row in rows_athletes_events:
+        if row[header_athletes_events["NOC"]] == noc and row[header_athletes_events["Year"]] == str(year):
+            try:
+                medals[row[header_athletes_events["Medal"]]] += 1
+                if not row[header_athletes_events["Name"]] in winners:
+                    winners.append(row[header_athletes_events["Name"]])
+            except KeyError:
+                continue
+    print(medals)
+    print(winners[:10])
+get_medals("USA", 2004)
