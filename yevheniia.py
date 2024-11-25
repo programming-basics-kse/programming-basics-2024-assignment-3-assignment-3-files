b3ef90ca161e5343c8ce3348c4ef925c5e2cae34
ysobko <y.sobko@kse.org.ua>
2024-11-25 10:45:01 +0200

    Update yevheniia.py

--- yevheniia.py
+++ yevheniia.py
@@ -1,14 +1,24 @@
 import sys
 
-def output_medals(results, country, year, output_file=None):
-    output_text = ""
+def output_total(results, year, output_file=None):
+    medal_counts = {}
+
     for row in results:
-        if (row["Team"] == country) and int(row["Year"]) == year:
-            output_text += (f"Country: {row['Team']}, Medals: {row['Gold']} gold, {row['Silver']} silver, "
-                            f"{row['Bronze']} bronze\n")
+        if int(row["Year"]) == year:
+            country = row["Team"]
+            if country not in medal_counts:
+                medal_counts[country] = {"Gold": 0, "Silver": 0, "Bronze": 0}
+            medal_counts[country]["Gold"] += int(row["Gold"])
+            medal_counts[country]["Silver"] += int(row["Silver"])
+            medal_counts[country]["Bronze"] += int(row["Bronze"])
+
+    output_text = ""
+    for country, counts in medal_counts.items():
+        output_text += (f"{country} - {counts['Gold']} gold - {counts['Silver']} silver"
+                        f" - {counts['Bronze']} bronze\n")
 
     if not output_text:
-        output_text = "No data for this country and year.\n"
+        output_text = "No data for this year.\n"
 
     if output_file:
         with open(output_file, "w", encoding="utf-8") as file:
