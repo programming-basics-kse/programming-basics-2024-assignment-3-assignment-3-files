d1d518bf2dc9fbd2bda48cfb75653e7b553f56bc
misha <makovkamisha10@gmail.com>
2024-11-27 10:31:38 +0200

    Output information to the console

--- main.py
+++ main.py
@@ -32,3 +32,26 @@ def medals(input_file, country, year) -> list[list]:
                 break
             counter += 1
         return medalists
+
+def output():
+    medals_result = medals(args.input_file, args.medals[0], args.medals[1])
+    if len(medals_result) == 0:
+        print('No medalists found')
+        print('Please enter the correct country or year.')
+        exit()
+
+    for i in range(len(medals_result)):
+        print(f'{i+1}. ', end='')
+        print(medals_result[i][0], medals_result[i][1], medals_result[i][1], end='', sep=' - ')
+        print()
+
+    gold = 0
+    silver = 0
+    bronze = 0
+    for i in medals_result:
+        gold += i.count('Gold')
+        silver += i.count('Silver')
+        bronze += i.count('Bronze')
+    print(f'Gold: {gold}')
+    print(f'Silver: {silver}')
+    print(f'Bronze: {bronze}')
