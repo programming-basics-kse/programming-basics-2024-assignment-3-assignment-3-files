00530f555b9ba3c10ed2224e3659556867a29c54
misha <makovkamisha10@gmail.com>
2024-11-27 11:14:09 +0200

    Added main function and made some minor changes

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import argparse
 
 MEDALISTS = 10
 
-parser = argparse.ArgumentParser(description='Medalists database processing program')
-parser.add_argument('input_file', help='Data file address')
-parser.add_argument('-medals', nargs=2, metavar = ('COUNTRY', 'YEAR'), help='The top ten medalists from this country at a given Olympiad')
-parser.add_argument('-output', metavar='NAME', help='The output file')
-args = parser.parse_args()
+def parser_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Medalists database processing program')
+    parser.add_argument('input_file', help='Data file address')
+    parser.add_argument('-medals', nargs=2, metavar = ('COUNTRY', 'YEAR'), help='The top ten medalists from this country at a given Olympiad')
+    parser.add_argument('-output', metavar='NAME', help='The output file')
+    return parser.parse_args()
 
 def medals(input_file, country, year) -> list[list]:
-    with open(args.input_file, 'rt') as file:
+    with open(input_file, 'rt') as file:
         counter = 0
         medalists = []
         next(file)
@@ -37,21 +38,20 @@ def output_file(output_file_name: str, content: str):
     with open(output_file_name, 'wt') as file:
         file.write(content)
 
-def output():
-    medals_result = medals(args.input_file, args.medals[0], args.medals[1])
+def output(medals_out: list[list], output_file_name: str):
     output_content = ''
 
-    if len(medals_result) == 0:
+    if len(medals_out) == 0:
         output_content = 'No medalists found\nPlease enter the correct country or year.'
     else:
-        for i in range(len(medals_result)):
+        for i in range(len(medals_out)):
             output_content += f'{i+1}. '
-            output_content += f'{medals_result[i][0]} - {medals_result[i][1]} - {medals_result[i][2]}'
+            output_content += f'{medals_out[i][0]} - {medals_out[i][1]} - {medals_out[i][2]}'
             output_content += '\n'
         gold = 0
         silver = 0
         bronze = 0
-        for i in medals_result:
+        for i in medals_out:
             gold += i.count('Gold')
             silver += i.count('Silver')
             bronze += i.count('Bronze')
@@ -60,5 +60,13 @@ def output():
         output_content += f'Bronze: {bronze}'
 
     print(output_content)
-    if args.output is not None:
-        output_file(args.output, output_content)
+    if output_file_name is not None:
+        output_file(output_file_name, output_content)
+
+def main():
+    args = parser_arguments()
+    medals_result = medals(args.input_file, args.medals[0], args.medals[1])
+    output(medals_result, args.output)
+
+if __name__ == '__main__':
+    main()
